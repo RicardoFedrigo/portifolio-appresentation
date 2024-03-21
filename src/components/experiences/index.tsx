25170d3759f15e5d3bd4ae2bfe762f0b3ec4a728
Ricardo Fedrigo <ricardo.fedrigo1995@gmail.com>
2024-03-21 19:08:54 -0300

    fix: corrigindo experiencias

--- src/components/experiences/index.tsx
+++ src/components/experiences/index.tsx
@@ -6,42 +6,23 @@ export function Experiencies() {
       </div>
       <br />
       <div>
-        <div className='job-name mb-6 font-bold mb-6 font-bold'>
-          <strong>Grupo Integrado </strong>Campo Mourão Estágio / programador – setembro 2019 –
-          junho 2021{" "}
+        <div className='job-name mb-6 font-bold'>
+          <strong>Konatus </strong>PJ / Full Stack – Dezembro 2023 - atualmente{" "}
         </div>
         <div className='job-description'>
-          No grupo integrado de educação realizei a criação de API's em Node.js no padrão REST para
-          suprir a demanda de cadastro, matrícula, gerenciamento de notas, utilizando ferramentas
-          como: express, joi, sequelize, lodash e para o armazenamento SQL Server. Como front-end
-          atuei criando e ajustando bugs em páginas web com responsividade em Vue.js e bootstrap
-          para a estilização e serve-side reder com PHP 5.6 e Jquery para realizar aos request para
-          back-end. Nesse período também realizei a criação de scripts em python 3, e atualização e
-          otimização tabelas em SQL
+          Contratado inicialmente como desenvolvedor back-end Node.js, tive a oportunidade de
+          demonstrar minha adaptabilidade e versatilidade ao assumir responsabilidades em projetos
+          diversos. Além das minhas habilidades no ecossistema Node.js, fui desafiado a expandir meu
+          conhecimento para incluir linguagens e frameworks como JAVA com Spring Boot e Angular.
+          Essa experiência enriquecedora fortaleceu minha capacidade de aprendizado rápido e me
+          permitiu contribuir de forma eficaz em ambientes multidisciplinares, mostrando minha
+          versatilidade e resiliência
         </div>
       </div>
       <br />
       <div>
         <div className='job-name mb-6 font-bold'>
-          <strong>Tecnospeed </strong>Tempo integral / programador back-end – junho 2021 – junho
-          2022 junho 2021{" "}
-        </div>
-        <div className='job-description'>
-          Atuei como back-end na criação e manutenção de api’s micro-serviços no padrão REST
-          utilizando conceitos de SOLID, algumas API's desenvolvidas foram: Criação de API para
-          integrar com múltiplos marketplaces utilizando Nest.js e typescript realizando o cache com
-          Redis e salvando as informações em um banco Postgres com framework Typeorm, a API era
-          hospedada na AWS integradas com serviços como SQS, Cloud Watch, S3, e para garantir a
-          qualidade do código realizado teste unitários e de integração com jest, superest e
-          faker-br. Criação de micro-serviço para processar múltiplas requisições de diferente
-          canais com Typescript redis e Bull.js
-        </div>
-      </div>
-      <br />
-      <div>
-        <div className='job-name mb-6 font-bold'>
-          <strong>Objective </strong>Tempo integral / Full Stack – junho 2022 – setembro 2023 junho
-          2021{" "}
+          <strong>Objective </strong>Tempo integral / Full Stack – junho 2022 – setembro 2023 2021{" "}
         </div>
         <div className='job-description'>
           Atuei primeiramente com foco no back-end, porém acabei me tornando Fullstack. No back-end
@@ -56,16 +37,34 @@ export function Experiencies() {
       <br />
       <div>
         <div className='job-name mb-6 font-bold'>
-          <strong>Konatus </strong>PJ / Full Stack – Dezembro 2023 - atualmente 2021{" "}
+          <strong>Tecnospeed </strong>Tempo integral / Programador back-end – junho 2021 – junho
+          2022 2022 junho 2021{" "}
         </div>
         <div className='job-description'>
-          Contratado inicialmente como desenvolvedor back-end Node.js, tive a oportunidade de
-          demonstrar minha adaptabilidade e versatilidade ao assumir responsabilidades em projetos
-          diversos. Além das minhas habilidades no ecossistema Node.js, fui desafiado a expandir meu
-          conhecimento para incluir linguagens e frameworks como JAVA com Spring Boot e Angular.
-          Essa experiência enriquecedora fortaleceu minha capacidade de aprendizado rápido e me
-          permitiu contribuir de forma eficaz em ambientes multidisciplinares, mostrando minha
-          versatilidade e resiliência
+          Atuei como back-end na criação e manutenção de api’s micro-serviços no padrão REST
+          utilizando conceitos de SOLID, algumas API's desenvolvidas foram: Criação de API para
+          integrar com múltiplos marketplaces utilizando Nest.js e typescript realizando o cache com
+          Redis e salvando as informações em um banco Postgres com framework Typeorm, a API era
+          hospedada na AWS integradas com serviços como SQS, Cloud Watch, S3, e para garantir a
+          qualidade do código realizado teste unitários e de integração com jest, superest e
+          faker-br. Criação de micro-serviço para processar múltiplas requisições de diferente
+          canais com Typescript redis e Bull.js
+        </div>
+      </div>
+      <br />
+      <div>
+        <div className='job-name mb-6 font-bold mb-6 font-bold'>
+          <strong>Grupo Integrado </strong>Campo Mourão Estágio / Estágio – setembro 2019 –
+          junho 2021{" "}
+        </div>
+        <div className='job-description'>
+          No grupo integrado de educação realizei a criação de API's em Node.js no padrão REST para
+          suprir a demanda de cadastro, matrícula, gerenciamento de notas, utilizando ferramentas
+          como: express, joi, sequelize, lodash e para o armazenamento SQL Server. Como front-end
+          atuei criando e ajustando bugs em páginas web com responsividade em Vue.js e bootstrap
+          para a estilização e serve-side reder com PHP 5.6 e Jquery para realizar aos request para
+          back-end. Nesse período também realizei a criação de scripts em python 3, e atualização e
+          otimização tabelas em SQL
         </div>
       </div>
     </div>
